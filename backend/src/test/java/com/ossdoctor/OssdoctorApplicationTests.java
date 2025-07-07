e8da45e05bd33987a2b2607933f040620e7cb0ae
Hyeri-hci <hyeri.hci.du@gmail.com>
2025-07-07 17:34:50 +0900

    Initialize backend and frontend folder structure

---
+++ backend/src/test/java/com/ossdoctor/OssdoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ossdoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssdoctorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
