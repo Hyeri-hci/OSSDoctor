f4b1a9f0706831b457d96aa7784206379d793382
hyeri-hci <hyeri.hci.du@gmail.com>
2025-07-16 05:24:21 +0900

    [Add] OSSDoctor HeaderSection Component 구현

--- frontend/src/features/MainPage/pages/MainPage.jsx
+++ frontend/src/features/MainPage/pages/MainPage.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-
+import HeaderSection from "../../../components/common/HeaderSection.jsx";
 export default function MainPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-primary">
-      <h1 className="text-4xl font-bold mb-4 text-red-600">Main Page</h1>
-      <p className="text-lg text-accent text-blue-600">Tailwind 정상 적용 확인.</p>
+    <div className="max-w-screen-2xl mx-auto flex flex-col items-center justify-center">
+      <HeaderSection />      
     </div>
   );
 }
