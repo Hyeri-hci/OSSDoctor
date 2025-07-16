e4bcb9107a3945a76987364b17e66c8231ba93e1
Hyeri-hci <hyeri.hci.du@gmail.com>
2025-07-16 16:47:31 +0900

    [Update] Footer Layout sm breakpoint 대응

--- frontend/src/components/common/FooterSection.jsx
+++ frontend/src/components/common/FooterSection.jsx
@@ -10,15 +10,14 @@ const FooterSection = () => {
 
   return (
       <footer className="w-full bg-[#FCFCFD] border-t border-gray-200 py-6">
-          <div className="flex flex-wrap items-center justify-center gap-8">
-              
+          <div className="flex flex-col sm:flex-row flex-wrap items-center justify-center gap-8">
               {/* Left: Copyright */}
-              <p className="text-xs text-[#575757]">
+              <p className="text-xs text-[#575757] order-2 sm:order-none">
                 © 2025 OSSDoctor. All rights reserved.
               </p>
 
               {/* Right: Links */}
-              <nav className="flex flex-wrap gap-4 justify-center">
+              <nav className="flex flex-wrap gap-4 justify-center order-1 sm:order-none">
                   {footerLinks.map((link, index) => (
                       <a
                           key={index}
@@ -30,6 +29,8 @@ const FooterSection = () => {
                       </a>
                   ))}
               </nav>
+
+              
           </div>
           
     </footer>
