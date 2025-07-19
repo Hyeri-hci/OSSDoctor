64c7721a0d2f9b32c2023842fd8a681f8932f4ea
Jeong Yunhwan <ok0528ok0@gmail.com>
2025-07-19 17:06:01 +0900

    [Add] 메인페이지 GitHub OAuth 로그인 이동 임시 생성

--- frontend/src/features/MainPage/pages/MainPage.jsx
+++ frontend/src/features/MainPage/pages/MainPage.jsx
@@ -5,6 +5,7 @@ export default function MainPage() {
     <div className="flex flex-col items-center justify-center h-screen bg-primary">
       <h1 className="text-4xl font-bold mb-4 text-red-600">Main Page</h1>
       <p className="text-lg text-accent text-blue-600">Tailwind 정상 적용 확인.</p>
+        <a href={"https://github.com/login/oauth/authorize?client_id=Iv23li5SWPbWBI5N68H8"}>github 로그인 이동</a>
     </div>
   );
 }
