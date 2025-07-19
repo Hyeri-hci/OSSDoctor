657e9cbcb4f6b6cc282cd67d224decb813cde9fd
hyeri-hci <hyeri.hci.du@gmail.com>
2025-07-19 19:10:26 +0900

    [Add] Diagnose, MyActivity, EcoSystem 임시 페이지 및 라우터 추가

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Importing the page components
 import MainPage from "../features/MainPage/pages/MainPage";
+import DiagnosePage from "../features/Diagnose/pages/DiagnosePage";
+import MyActivityPage from "../features/MyActivity/pages/MyActivityPage";
+import EcosystemPage from "../features/Ecosystem/pages/EcosystemPage";
 
 export default function AppRouter() {
   return (
@@ -10,6 +13,18 @@ export default function AppRouter() {
       <Routes>
         {/* Main Page Route Path */}
         <Route path="/" element={<MainPage />} />
+
+        {/* Diagnose Page Route Path */}
+        <Route path="/diagnose" element={<DiagnosePage />} />
+
+        {/* My Activity Page Route Path */}
+        <Route path="/myactivity" element={<MyActivityPage />} />
+
+        {/* Ecosystem Page Route Path */}
+        <Route path="/ecosystem" element={<EcosystemPage />} />
+
+        {/* GitHub OAuth Callback Route Path */}
+        
       </Routes>
     </BrowserRouter>
   );
