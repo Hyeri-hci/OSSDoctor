75f1c31013442a6b056557e0ec90e73d35e6e9ee
hyeri-hci <hyeri.hci.du@gmail.com>
2025-07-20 01:55:17 +0900

    [Add] 공통 UI 컴포넌트(진단, 기여도 페이지) 탭 이동 카드 생성 -- 추후 변경 가능

--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -4,3 +4,4 @@ export { default as Card } from './Card';
 export { default as SearchBar } from './SearchBar';
 export { default as Badge } from './Badge';
 export { default as Navigation } from './Navigation';
+export { default as ScoreCards } from './ScoreCards';
