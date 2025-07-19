55080e695ff0dba17bd4bb42d510c7ab2f197bab
hyeri-hci <hyeri.hci.du@gmail.com>
2025-07-19 17:56:55 +0900

    [Add] 공통 UI 컴포넌트 Navigation 생성

--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -3,3 +3,4 @@ export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as SearchBar } from './SearchBar';
 export { default as Badge } from './Badge';
+export { default as Navigation } from './Navigation';
