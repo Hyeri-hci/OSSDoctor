cdc648d14edc497e0a724276f9472f4f020c3b28
DABIN\dabin <dabin32001480@gmail.com>
2025-07-21 16:17:33 +0900

    [ADD] Level 엔티티 및 기본 구성 추가 (DTO, Repository, Service 포함)

---
+++ backend/src/main/java/com/ossdoctor/Service/LevelService.java
@@ -0,0 +1,37 @@
+package com.ossdoctor.Service;
+
+import com.ossdoctor.DTO.LevelDTO;
+import com.ossdoctor.Entity.LevelEntity;
+import com.ossdoctor.Repository.LevelRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Level;
+
+@Service
+@AllArgsConstructor
+public class LevelService {
+
+    private LevelRepository levelRepository;
+
+    private LevelDTO save(LevelDTO levelDTO) {
+        return toDTO(levelRepository.save(toEntity(levelDTO)));
+    }
+
+    private LevelDTO toDTO(LevelEntity entity) {
+        return LevelDTO.builder()
+                .idx(entity.getIdx())
+                .levelId(entity.getLevelId())
+                .title(entity.getTitle())
+                .requiredExp(entity.getRequiredExp())
+                .build();
+    }
+
+    private LevelEntity toEntity(LevelDTO dto) {
+        return LevelEntity.builder()
+                .levelId(dto.getLevelId())
+                .title(dto.getTitle())
+                .requiredExp(dto.getRequiredExp())
+                .build();
+    }
+}
