0451f1cad80a57ae845d0fffabee3b403d76f0d8
DABIN\dabin <dabin32001480@gmail.com>
2025-07-21 15:55:53 +0900

    [ADD] Contribution 엔티티 및 기본 구성 추가 (DTO, Repository, Service 포함)

--- backend/src/main/java/com/ossdoctor/Entity/ContributionEntity.java
+++ backend/src/main/java/com/ossdoctor/Entity/ContributionEntity.java
@@ -1,4 +1,42 @@
 package com.ossdoctor.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "contribution")
+@Entity
 public class ContributionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "repository_id", nullable = false)
+    private RepositoryEntity repository;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "reference_type", nullable = false)
+    private REFERENCE_TYPE referenceType;
+
+    @Column(name = "reference_id", nullable = false)
+    private Long referenceId;
+
+    @Column(name = "contributed_at", nullable = false)
+    private LocalDateTime contributedAt;
+
+    private String description;
 }
