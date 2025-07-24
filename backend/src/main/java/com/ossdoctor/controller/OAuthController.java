2e244920e2cb1f60be363f34439eba60593c4b16
Jeong Yunhwan <ok0528ok0@gmail.com>
2025-07-24 18:40:25 +0900

    [Add] jwt 생성, 리다이렉트 주소 수정

--- backend/src/main/java/com/ossdoctor/controller/OAuthController.java
+++ backend/src/main/java/com/ossdoctor/controller/OAuthController.java
@@ -1,5 +1,7 @@
 package com.ossdoctor.controller;
 
+import com.nimbusds.jose.shaded.gson.JsonObject;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
@@ -12,16 +14,32 @@ import org.springframework.web.client.RestTemplate;
 import java.net.URI;
 import java.util.List;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
 @RestController
 public class OAuthController {
     @Value("${spring.security.oauth2.client.registration.github.client-id}")
     private String clientId;
     @Value("${spring.security.oauth2.client.registration.github.client-secret}")
     private String clientSecret;
+    @Value("${jwt.secret.key}")
+    private String jwtSecret;
+    // JWT 비밀 키
+
+
+
 
     @GetMapping("/oauth/callback")
     public ResponseEntity<?> githubCallback(@RequestParam("code") String code) {
-        System.out.println("코드 잘 받았다." + code);
+        Key jwtkey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+        // 세션 시간
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + 3600000);
 
         // 깃허브 access token 요청 (폼 데이터 전송)
         RestTemplate restTemplate = new RestTemplate();
@@ -33,26 +51,66 @@ public class OAuthController {
         params.add("code", code);
 
         HttpHeaders tokenHeaders = new HttpHeaders();
-        tokenHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED); // 반드시 form으로!
-        tokenHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));         // 응답 json 기대
+        tokenHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        tokenHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
 
+        // HttpEntity 생성
         HttpEntity<MultiValueMap<String, String>> tokenRequest =
                 new HttpEntity<>(params, tokenHeaders);
 
-        ResponseEntity<String> response = restTemplate.postForEntity(
+        // 위에서 생성한 HttpEntity를 아래의 url로 전달해서 결과를 받아온다. 결과에는 access token이 있다.
+        ResponseEntity<String> oauthResponse = restTemplate.postForEntity(
                 "https://github.com/login/oauth/access_token",
                 tokenRequest,
                 String.class
         );
 
-        System.out.println("Access Token 응답: " + response.getBody());
+        // 결과를 JSONObject로 받아오기
+        String oauthResult = oauthResponse.getBody();
+        JSONObject jsonObject = new JSONObject(oauthResult);
 
-        // 여기서 원하는 후속 처리 가능 (예: access_token 파싱)
-        // ... (optionally access_token 추출해서 저장, 로그 등)
+        // JSONObjec에서 access token을 확인해서 저장하기
+        String accessToken = jsonObject.get("access_token").toString();
+
+
+        System.out.println("Access Token 응답: " + accessToken);
+
+        // 사용자 정보 받아오기
+        HttpHeaders userheaders = new HttpHeaders();
+        userheaders.set("Accept", "application/vnd.github+json");
+        userheaders.set("Authorization", "Bearer " + accessToken);
+        userheaders.set("X-GitHub-Api-Version", "2022-11-28");
+
+        HttpEntity<String> entity = new HttpEntity<>(userheaders);
+        RestTemplate userRestTemplate = new RestTemplate();
+        ResponseEntity<String> userResponse = userRestTemplate.exchange(
+                "https://api.github.com/user",
+                HttpMethod.GET,
+                entity,
+                String.class
+        );
+
+        String userResult = userResponse.getBody();
+        System.out.println(userResult);
+
+        JSONObject userJson = new JSONObject(userResult);
+        String nickname = userJson.getString("login");
+        String jwt = Jwts.builder()
+                .setSubject(String.valueOf(userJson.getInt("id")))
+                .claim("nickname", nickname)
+                .claim("avatar_url",userJson.getString("avatar_url"))
+                .claim("bio", userJson.optString("bio"))
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(jwtkey, SignatureAlgorithm.HS256)
+                .compact();
+
+        System.out.println("\n\n\n\n\n" + jwt);
+
 
         // 리다이렉트 응답
         HttpHeaders redirectHeaders = new HttpHeaders();
-        redirectHeaders.setLocation(URI.create("http://localhost:5173")); // 프론트 주소
+        redirectHeaders.setLocation(URI.create("http://localhost:5173/?auth=success&user=" + nickname)); // 프론트 주소
         return ResponseEntity.status(HttpStatus.FOUND)
                 .headers(redirectHeaders)
                 .build();
