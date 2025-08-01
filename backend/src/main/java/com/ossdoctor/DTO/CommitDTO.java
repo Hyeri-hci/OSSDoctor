4d4153109220c2b235c7dcb36f3c023fdb5b0d27
DAEGU\22221578 <dabin32001480@gmail.com>
2025-08-01 18:18:14 +0900

    [Update] CommitDTO 수정

--- backend/src/main/java/com/ossdoctor/DTO/CommitDTO.java
+++ backend/src/main/java/com/ossdoctor/DTO/CommitDTO.java
@@ -1,22 +1,16 @@
 package com.ossdoctor.DTO;
 
-import com.ossdoctor.Entity.RepositoryEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommitDTO {
-    private Long idx;
-    private Long repositoryId;
-    private Long userId;
-    private String sha;
-    private String message;
-    private LocalDateTime committedAt;
+    private String date;      // yyyy-MM-dd 형식 문자열
+    private int commits;      // 그 날짜의 커밋 수
+    private String day;
 }
