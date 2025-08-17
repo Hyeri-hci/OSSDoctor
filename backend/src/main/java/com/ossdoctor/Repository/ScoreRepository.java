e4cc578daa414664ee54e7aa7086978d720a3a71
hyeri-hci <hyeri.hci.du@gmail.com>
2025-08-18 00:48:25 +0900

    [Update] 저장소 ID 기준으로 점수 로직 관련 Repository, Service 수정

--- backend/src/main/java/com/ossdoctor/Repository/ScoreRepository.java
+++ backend/src/main/java/com/ossdoctor/Repository/ScoreRepository.java
@@ -4,7 +4,9 @@ import com.ossdoctor.Entity.ScoreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ScoreRepository extends JpaRepository<ScoreEntity, Long> {
-
+    List<ScoreEntity> findByRepositoryIdx(Long repositoryId);
 }
