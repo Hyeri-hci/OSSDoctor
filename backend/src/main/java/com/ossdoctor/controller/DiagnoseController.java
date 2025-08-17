01549a55f1838aa17fc30c611ddf1a59dcaa8d5d
hyeri-hci <hyeri.hci.du@gmail.com>
2025-08-18 00:51:46 +0900

    [Refactor] 사용하지 않는 import문 삭제

--- backend/src/main/java/com/ossdoctor/controller/DiagnoseController.java
+++ backend/src/main/java/com/ossdoctor/controller/DiagnoseController.java
@@ -2,7 +2,6 @@ package com.ossdoctor.controller;
 
 import com.ossdoctor.DTO.RepositoryDTO;
 import com.ossdoctor.Service.GitHubApiService;
-import com.ossdoctor.Service.ScoreService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
